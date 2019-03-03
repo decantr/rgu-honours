785fcf8b1b03d540e76362e1e98dacdf1fc267df
decantr <theeasterlywood@gmail.com>
2019-03-03 12:51:45 +0000

    cannot add multidimensional values into url Values

--- read.go
+++ read.go
@@ -2,36 +2,31 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
-	"bytes"
+	"net/url"
 )
 
 func main() {
 
-	url := "http://localhost:8086"
+	turl := "http://localhost:8086/write?db=test"
 
-	var jpacket = []byte(`[{
-            'measurement': 'test',
-            'tags': {
-                "host": xps
-            },
-            'fields': {
-                'cpu_usage': 100
-            }
-        }]`)
+	res, err := http.PostForm(turl,
+		url.Values{"measurement": {"usage"}})
 
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jpacket))
+	//  "tags": url.Values{"host": \"xps"}, "fields": url.Values{"cpu_usage": "100"}})
 
-	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
+	//	res, err := http.PostForm("http://duckduckgo.com",
+	//		url.Values{"q": {"github"}})
 
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	defer resp.Body.Close()
-	fmt.Println("response Status:", resp.Status)
-	fmt.Println("response Headers:", resp.Header)
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	fmt.Println("p:\n", string(body), 3)
+	fmt.Println("response Status:", res.Status)
+	fmt.Println("response Headers:", res.Header)
 }
